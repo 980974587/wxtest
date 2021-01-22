8ee64c4ed0c116a3b2b7cb6adc40428365c7f20e
ink <980974587@qq.com>
2021-01-22 17:03:37 +0800

    忽略文件

---
+++ mytest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('Util.php');
+define("ROOT",__DIR__);
+
+
+//echo1111;
+
+$abc=1;
+
+$abc=22;
+echo $abc;
+
--- test.php
+++ test.php
@@ -1,17 +1,17 @@
 <?php
-// phpinfo();
+phpinfo();
 
-$appid='wx3f6c84918123741f';
-$secret='9eebefa92a78d8bd47cbb4fed8368bf4';
-$redirectUri='http://inkqx.nat300.top';
+// $appid='wx3f6c84918123741f';
+// $secret='9eebefa92a78d8bd47cbb4fed8368bf4';
+// $redirectUri='http://inkqx.nat300.top';
 
-$askUrl="https://open.weixin.qq.com/connect/oauth2/authorize?";
-$askParam=[
-    'appid'=>$appid,
-    'redirect_uri'=>$redirectUri,
-    'response_type'=>'code',
-    'scope'=>'snsapi_base',
-    'state'=>'test'
-];
-$askUrl.=http_build_query($askParam)."#wechat_redirect";
-header("Location:".$askUrl);
+// $askUrl="https://open.weixin.qq.com/connect/oauth2/authorize?";
+// $askParam=[
+//     'appid'=>$appid,
+//     'redirect_uri'=>$redirectUri,
+//     'response_type'=>'code',
+//     'scope'=>'snsapi_base',
+//     'state'=>'test'
+// ];
+// $askUrl.=http_build_query($askParam)."#wechat_redirect";
+// header("Location:".$askUrl);
